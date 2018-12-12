449d3286d326e30f7cffcb42160454be4e81c4f5
andrespaez16 <kinkbmx216@gmail.com>
2018-12-11 23:02:18 -0500

    defenicion de metodos en controladores y configuracion de rutas

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,14 +1,24 @@
 class RatesController < ApplicationController
 def index
-
-end   
+  @rates=Rate.all 
+end
+  
 
 def new
-
+@rate=Rate.new
  
 end
  
 def create
+    @rate = Rate.new(rate_params)
+    if @rate.save
+      redirect_to "index"
 
+       end
 end
+
+private
+      def rate_params
+        params.require(:rate).permit(:type_vehicle,:rate)
+      end
 end
--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,2 +1,25 @@
 class VehiclesController < ApplicationController
+
+    def index
+      @vehicles=Vehicle.all  
+    end
+
+    def new
+     @rates= Rate.all
+     @vehicle=Vehicle.new 
+    end
+
+    def create
+        @vehicle = Vehicle.new(vehicle_params)
+        if @vehicle.save
+
+
+          redirect_to "index"
+    
+           end
+    end
+    private
+      def vehicle_params
+        params.require(:vehicle).permit(:plate,:rate_id)
+      end
 end
